cb2c02efc524171e7cc030051ff2bb0e0618efd0
Melissa Preece <melissanchez@gmail.com>
2019-11-23 11:29:26 -0800

    Updated a working version of T SHIRT INFO section

--- interactive-form-v1/js/script.js
+++ interactive-form-v1/js/script.js
@@ -1,6 +1,6 @@
 /******************************************
 Treehouse Techdegree:
-FSJS project 3 - Form Validation
+FSJS project 3 - Interactive Form
 ******************************************/
 
 
@@ -8,28 +8,74 @@ FSJS project 3 - Form Validation
 // On page load, cursor appears in 'Name' field
 $("#name").focus();
 
+
 //JOB ROLE
-//Initially hide "Job Role: Other" input field and show when JavaScript is disabled
+//Initially hide "Other Job Role" input field and show when JavaScript is disabled
 $("#other-title").hide();
-//When "Job Role: Other" is selected, "Other input field" is displayed
-//Add event listener to the "other" <option> element:
-    //Select the element then pass in event method
-$("#title option:last").on('selected', function(){
-    $("#other-title").show();
+
+//When "Job Role:Other" is chosen from select element, show "Other Job Role" input text field
+$("select#title").on("change", function(event){
+    let $selectedValue = $(this).val();
+    const $other = $("select#title option:last").val();
+    if ($selectedValue === $other) {
+        $("#other-title").show();
+    } else {
+        $("#other-title").hide();
+    };
 });
 
+
 //T-SHIRT INFO
-//Hide 'Select theme' option from 'Design menu'
-$("select#design option:first").hide();
+const $designSelectElement = $("select#design");
+const $colorSelectElement = $("select#color");
+const $designOptions = $("select#design option");
+const $colorOptions = $("select#color option");
+const $themeOption = $("select#design option").eq(0);
+const $selectShirtTheme = $("<option>Please select a T-shirt theme</option>");
+
+//Display 'Select theme' option as default from 'Theme menu'
+$themeOption.attr("selected", true);
+
 //Update the 'Color' option to read 'Please select a T-shirt theme'
-$("select#color").prepend($('<option>Please select a T-shirt theme.</option>').val("pleaseselecttshirt"));
-$(document).ready(function() {
-    $("select#color option").filter(function() {
-        return $(this).val() == "pleaseselecttshirt"; 
-    }).prop('selected', true);
-    $("select#color option").hide();
-});
-//When one of two themes selected, only appropriate colors will show.
+$colorSelectElement.prepend($selectShirtTheme);
+$selectShirtTheme.attr("selected", true);
+$colorOptions.hide();
+
+//Initiate change event listener on 'Theme menu'
+$designSelectElement.on("change", (function(event) {
+    //When one of two themes is selected, only appropriate colors will show.
+    if ($(event.target).val() === "js puns") {
+        //Disable 'Select theme' option from 'Theme menu'
+        $themeOption.attr("disabled", "hidden");
+        //Disable 'Please select tshirt theme' from 'Color menu'
+        $selectShirtTheme.attr("disabled", "hidden");
+        //Show ONLY first 3 color options from 'Color menu'
+        $colorOptions.eq(0).attr("selected", true).show();
+        $colorOptions.eq(1).show();
+        $colorOptions.eq(2).show();
+        $colorOptions.eq(3).removeAttr("selected").hide();
+        $colorOptions.eq(4).hide();
+        $colorOptions.eq(5).hide();
+    } else if ($(event.target).val() === "heart js") {
+        //Disable 'Select theme' option from 'Theme menu'
+        $themeOption.attr("disabled", "hidden");
+        //Disable 'Please select tshirt theme' from 'Color menu'
+        $selectShirtTheme.attr("disabled", "hidden");
+        //Show ONLY last 3 color options from 'Color menu'
+        $colorOptions.eq(0).removeAttr("selected").hide();
+        $colorOptions.eq(1).hide();
+        $colorOptions.eq(2).hide();
+        $colorOptions.eq(3).attr("selected", true).show();
+        $colorOptions.eq(4).show();
+        $colorOptions.eq(5).show();
+    };
+}));
+
+console.log($designOptions);
+console.log($colorOptions);
+
+
+//ACTIVITY REGISTRATION
 
 
 
