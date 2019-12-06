a4d6fd9d6ff94d4a9b5400496164a0423ea0b0df
Melissa Preece <melissanchez@gmail.com>
2019-12-05 21:38:36 -0800

    Update .css and .js files
    CSS:
    -Added style for class "error"
    
    JS:
    -T-Shirt info - tested and works
    -Activities info - tested and works
    -Payment info - tested and works
    -Form validation - validation issue with Activities checkboxes

--- interactive-form-v1/js/script.js
+++ interactive-form-v1/js/script.js
@@ -71,12 +71,172 @@ $designSelectElement.on("change", (function(event) {
     };
 }));
 
-console.log($designOptions);
-console.log($colorOptions);
+//console.log($designOptions);
+//console.log($colorOptions);
 
 
 //ACTIVITY REGISTRATION
+//Disable  Conflicting Activities
+//Declare variable to hold ALL checkboxes
+const checkboxes = $("[type=checkbox]");
+//console.log(checkboxes);
+
+//Update and Display TOTAL Activity Cost
+//Create DOM element and append to '.activities'
+let p = $("<p></p>").appendTo(".activities");
+let totalActivityCost = 0;
+
+//Initiate change event listener on parent fieldset "Activities" element
+$(".activities").on ("change", (function (event) {
+    // Store clicked element
+    let clicked = event.target
+    // Store attribute 'data-day-and-time' of clicked element
+    let $clickedType = clicked.getAttribute("data-day-and-time");
+    // Store attribute 'data-cost' of clicked element
+    let $clickedCost = clicked.getAttribute("data-cost");
+    let $clickedCostNum = $clickedCost.replace("$", "");
+    //console.log(typeof $clickedCost);
+    //console.log($clickedCost);
+    //console.log(clicked);
+    //console.log($clickedType);
+
+    //If else conditional: if checkbox is checked, add cost of clicked activity to total cost, else subtract
+    if ($(clicked).prop("checked")) {
+        //Add cost of clicked activity to total cost variable
+        totalActivityCost = parseInt(totalActivityCost) + parseInt($clickedCostNum);
+        p.text("Total: $" + totalActivityCost);
+    } else {
+        //Subtract cost of clicked activity from total cost variable
+        totalActivityCost = parseInt(totalActivityCost) - parseInt($clickedCostNum);
+        p.text("Total: $" + totalActivityCost);
+    };
+    
+    //Initiate loop on ALL checkboxes
+    for (let i = 0; i < checkboxes.length; i += 1) {
+        //Declare variable to hold 'day and time' data attribute for each looped checkbox
+        let $checkboxType = checkboxes[i].getAttribute("data-day-and-time");
+        //If conditional: checkbox type equals clicked type AND checkbox in current loop is different from clicked checkbox
+        if ($checkboxType === $clickedType && checkboxes[i] !== clicked) {
+            //If else conditional: if both conditions met, and clicked checkbox is checked
+            if ($(clicked).prop("checked")) {
+                //Disable checkbox in current loop
+                $(checkboxes[i]).attr("disabled", true);
+            } else {
+                //Enable checkbox in current loop
+                $(checkboxes[i]).attr("disabled", false);
+            };
+        };
+    };
+    //console.log($clickedCostNum);
+    //console.log(p); 
+}));
+
+
+//PAYMENT
+//On page load, hide “Select Payment Method” `option` in drop down menu & only show Credit Card section
+$("select#payment option").eq(0).hide();
+$("div#paypal").hide();
+$("div#bitcoin").hide();
+//Get value of payment select element, and if it’s = ‘credit card’, set the credit card payment section to show, and set the other two options to hide.
+//Repeat for "Paypal" and "Bitcoin" sections
+$("select#payment").on ("change", function(event) {
+    if ($(event.target).val() === "Credit Card") {
+        $("div#credit-card").show();
+        $("div#paypal").hide();
+        $("div#bitcoin").hide();
+    } else if ($(event.target).val() === "PayPal") {
+        $("div#credit-card").hide();
+        $("div#paypal").show();
+        $("div#bitcoin").hide();
+    } else {
+        $("div#credit-card").hide();
+        $("div#paypal").hide();
+        $("div#bitcoin").show();
+    };
+});
+
+
+//FORM VALIDATION
+
+//Error message function
+const errorIndicator = function (element) {
+    $(element).addClass("error");
+    $(element).after($("<span></span>").attr("id", "error"));
+    $("span#error").text("Please fill in required field").css("color", "red");
+};
+
+//Validate NAME
+const validateName = function () {
+    let $inputName = $("input#name").val();
+    let submitName;
+    if ($inputName.length < 1) {
+        //Add error indicator and return false
+        errorIndicator("input#name");
+        submitName = false;
+    } else {
+        //Remove error indicator and return true
+        $("input#name").removeClass("error");
+        $("span#error").hide();
+        submitName = true;
+    };
+};
+
+//Validate EMAIL
+const validateEmail = function () {
+    let $inputEmail = $("input#mail").val();
+    const regEx = /^\S+@\S+$/;
+    let validEmail = regEx.test($inputEmail);
+    let submitEmail;
+    if ($inputEmail.length < 1 || !validEmail) {
+        //Add error indicator and return false
+        errorIndicator("input#mail");
+        submitEmail = false;
+    } else {
+        //Remove error indicator and return true
+        $("input#mail").removeClass("error");
+        $("span#error").hide();
+        submitEmail = true;
+    };
+};
+
+//Validate ACTIVITY
+const validateActivity = function () {
+    let submitActivity;
+    const $checkbox = $("[type=checkbox]");
+    //Loop through ALL checkbox elements
+    $checkbox.each(function () {
+        if ($checkbox.prop("checked")) {
+            submitActivity = true;
+            return submitActivity;
+        } else {
+            submitActivity = false;
+            return submitActivity;
+        };
+    });
+    if (submitActivity === false) {
+        //Add error indicator
+        $("p").after($("<span></span>").attr("id", "error"));
+        $("span#error").text("Please fill in required field").css("color", "red");
+    } else {
+        //Remove error indicator
+        $("span#error").hide();
+    };
+};
+
+//Create function to validate CREDIT CARD info IF payment option is selected
+
+    //Credit card
+
+    //Zip code
+
+    //CVV
 
 
 
-
+//Submit Event Listener
+$("form").on("submit", function (event) {
+    event.preventDefault();
+    validateName();
+    validateEmail();
+    validateActivity();
+});
